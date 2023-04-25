f12c5e2b6041bf08c95c885f0dceda00894a86b8
philip dutra <monsterphilip10@gmail.com>
2023-04-25 19:45:14 -0400

    updated index.js

--- index.js
+++ index.js
@@ -7,7 +7,66 @@
 
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
- */
+ */// Your code here
+function createEmployeeRecord(array){
+    let employee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+
+}
+
+function createEmployeeRecords(array){ 
+    let records = []
+    array.forEach(employee => records.push(createEmployeeRecord(employee)))
+    return records
+}
+
+function createTimeInEvent(stamp){
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: stamp.slice(0, -5),
+        hour: parseInt(stamp.slice(11))
+    })
+    return this
+}
+function createTimeOutEvent(stamp){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: stamp.slice(0, -5),
+        hour: parseInt(stamp.slice(11))
+    })
+    return this
+}
+
+function hoursWorkedOnDate(stamp){
+    let timeIn = this.timeInEvents.filter(obj => obj.date === stamp)
+    let timeOut = this.timeOutEvents.filter(obj => obj.date === stamp)
+    let hours = (timeOut[0].hour - timeIn[0].hour)/100
+    return hours
+}
+
+function wagesEarnedOnDate(date){
+    
+    return hoursWorkedOnDate.call(this, date)*this.payPerHour
+}
+
+function calculatePayroll(array){
+    let total = 0
+    array.forEach(employee => total+= allWagesFor.call(employee))
+    return total
+}
+
+function findEmployeeByFirstName(array, name){
+    let employee = array.filter(record => record.firstName === name)
+    return employee[0]
+}
+ 
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
